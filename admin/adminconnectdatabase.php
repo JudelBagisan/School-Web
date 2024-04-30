ec4a3d9a3da00b4c9473393ca2ff92933f95482a
Judel Bagisan <144524890+JudelBagisan@users.noreply.github.com>
2024-04-30 21:25:09 +0800

    Delete admin directory

--- admin/adminconnectdatabase.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost","root","","enrolldb");
-    
-    if ($conn->connect_error) {
-        die("Fatal error, unable to connect to database : ". $conn->connect_error);
-    }
-
-?>
